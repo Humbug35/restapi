a969eedc1aaaea35cdbe547f77e8759849ad14f7
Linus Plume <plumee_91@hotmail.com>
2018-10-19 08:23:31 +0200

    datalayer added

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 let createError = require('http-errors');
-const products = require('./articles.json');
-
+const Mongo = require('./DataLayer/mongo');
 const productsRoutes = require('./routes/products')
 
-mongoose.connect('mongodb+srv://admin:Omegapoint@cluster0-jtzfp.mongodb.net/BosseLinus?retryWrites=true', { useNewUrlParser: true });
-const db = mongoose.connection;
-
-db.once('open', () => {
-  console.log('Connected to MongoDB')
-})
-
-db.on('error', (error) => {
-  console.log(error)
-})
-
-
+let mongo = new Mongo();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.get('/', (req, res) => {
-  res.redirect('/products')
-})
-app.use('/products', productsRoutes);
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/products', productsRoutes);
+
+app.use('*', (req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
-  next(error);
+  res.status(error.status).json({
+    message: error.message,
+    status: error.status
+  })
+  next();
 })
 
 app.use((error, req, res, next) => {
@@ -43,31 +45,58 @@ app.use((error, req, res, next) => {
   })
 })
 
-
-
-
-
-let sendJsonFileToDatabase = false;
-function sendProductsToDatabase() {
-  if(sendJsonFileToDatabase) {
-    products.forEach(product => {
-      product = new Product({
-        _id: mongoose.Types.ObjectId(),
-        productNumber: product.id,
-        product_name: product.product_name,
-        price: product.price,
-        Category: product.Category.toLowerCase()
-      }).save()
-    })
-  } else {
-      console.log('False')
-  }
-}
-sendProductsToDatabase();
-
-
-
 const port = 5000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const products = require('./articles.json');
+//const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://admin:Omegapoint@cluster0-jtzfp.mongodb.net/BosseLinus?retryWrites=true', { useNewUrlParser: true });
+// const db = mongoose.connection;
+//
+// db.once('open', () => {
+//   console.log('Connected to MongoDB')
+// })
+//
+// db.on('error', (error) => {
+//   console.log(error)
+// })
+
+
+// let sendJsonFileToDatabase = false;
+// function sendProductsToDatabase() {
+//   if(sendJsonFileToDatabase) {
+//     products.forEach(product => {
+//       product = new Product({
+//         _id: mongoose.Types.ObjectId(),
+//         productNumber: product.id,
+//         product_name: product.product_name,
+//         price: product.price,
+//         Category: product.Category.toLowerCase()
+//       }).save()
+//     })
+//   } else {
+//       console.log('False')
+//   }
+// }
+// sendProductsToDatabase();
--- routes/products.js
+++ routes/products.js
@@ -1,67 +1,174 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Product = require('../models/productModel');
+const Mongo = require('../DataLayer/mongo');
 
+let mongo = new Mongo();
 
 router.get('/', (req, res, next) => {
   let skip = Number(req.query.skip);
   let limit = Number(req.query.limit);
   let category = req.query.category;
   let price = req.query.price;
-  Product.find()
-    .sort(price ? { price: price } : { _id: 1 })
-    .skip(skip)
-    .limit(limit)
-    .where(category ? Product.find({ 'Category': category }) : Product.find())
+  mongo.getAllProducts(skip, limit, price, category)
     .then(products => {
+      console.log('then')
       res.status(200).json(products)
+  })
+  .catch(() => {
+    next()
+  })
+});
+
+router.get('/:productId', (req, res, next) => {
+  const productId = req.params.productId;
+  mongo.getSingleProduct(productId)
+    .then(product => {
+      if(product) {
+          res.status(200).json(product)
+      } else {
+          next()
+      }
     })
     .catch(() => {
       next()
     })
 });
 
-router.get('/:productId', (req, res, next) => {
-  const productId = req.params.productId;
-  Product.findById(productId)
-    .then(product => {
-      if(product) {
-        res.status(200).json(product)
-      }
-    })
-    .catch(() => {
-      const error = new Error('Product Not Found');
-      error.status = 404;
-      next(error)
-    })
-});
-
 router.post('/', (req, res, next) => {
-  if(!req.body.product_name) {
-    res.status(403).send('Product Name is required')
-  } else if(!req.body.price) {
-      res.status(403).send('Price is required')
-  } else if(!req.body.Category) {
-      res.status(403).send('Category is required')
+  let body;
+  if(!req.body.product_name || '' && !req.body.price || '' && !req.body.Category || '') {
+      res.status(403).send('Some field is empty')
   } else {
-    let product = new Product({
-      _id: mongoose.Types.ObjectId(),
+    body = {
       product_name: req.body.product_name,
       price: req.body.price,
       Category: req.body.Category.toLowerCase()
-    })
-    product.save()
-      .then(
-        res.status(201).send({
-          message: 'Product Created',
-          product
-        })
-      )
-      .catch(error => {
-        res.status(400).send('ErrorMessage: ', error)
-      })
+    }
   }
+  mongo.postProduct(body)
+   .then(product => {
+     res.status(201).json({
+       message: 'Product Created',
+       product
+     })
+   })
+   .catch(() => {
+     next()
+   })
 });
 
+router.delete('/:productId', (req, res, next) => {
+  const productId = req.params.productId;
+  mongo.deleteProduct(productId)
+    .then(product => {
+      res.status(200).send(`Product with ID: ${productId} deleted`)
+    })
+    .catch(() => {
+      next()
+    })
+});
+
+router.patch('/:productId', (req, res, next) => {
+  mongo.patchProduct(req, res, next)
+});
+
+router.put('/:productId', (req, res, next) => {
+  mongo.updateProduct(req, res, next)
+})
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sendJsonFileToDatabase = false;
+// function sendProductsToDatabase() {
+//   if(sendJsonFileToDatabase) {
+//     products.forEach(product => {
+//       product = new Product({
+//         _id: mongoose.Types.ObjectId(),
+//         productNumber: product.id,
+//         product_name: product.product_name,
+//         price: product.price,
+//         Category: product.Category.toLowerCase()
+//       }).save()
+//     })
+//   } else {
+//       console.log('False')
+//   }
+// }
+// sendProductsToDatabase();
+
+
+
+  // let skip = Number(req.query.skip);
+  // let limit = Number(req.query.limit);
+  // let category = req.query.category;
+  // let price = req.query.price;
+  // Product.find()
+  //   .sort(price ? { price: price } : { _id: 1 })
+  //   .skip(skip)
+  //   .limit(limit)
+  //   .where(category ? Product.find({ 'Category': category }) : Product.find())
+  //   .then(products => {
+  //     res.status(200).json(products)
+  //   })
+  //   .catch(() => {
+  //     next()
+  //   })
+
+
+
+  // const productId = req.params.productId;
+  // Product.findById(productId)
+  //   .then(product => {
+  //     if(product) {
+  //       res.status(200).json(product)
+  //     }
+  //   })
+  //   .catch(() => {
+  //     const error = new Error('Product Not Found');
+  //     error.status = 404;
+  //     next(error)
+  //   })
